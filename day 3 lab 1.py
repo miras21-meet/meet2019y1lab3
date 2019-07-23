61f9b064bec0a8f5463af03c3ba237a4e8f28d50
miras21-meet <miras21@meet.mit.edu>
2019-07-23 10:08:27 +0300

    Stopping here

---
+++ day 3 lab 1.py
@@ -0,0 +1,7 @@
+print ('hello world')
+print ("hello world")
+user = input ("say something!")
+print ('say something'.upper())
+print ('SAY SOMETHING'.lower())
+print ('say SomEthIng'.swapcase())
+
---
+++ echo.py
@@ -0,0 +1,24 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('hello world')
+hello world
+>>> #print ('hello world')
+>>> 
+========= RESTART: /home/student/miras21_lab3/meet2019y1lab3/echo.py =========
+hello world
+>>> 
+>>> 
+========= RESTART: /home/student/miras21_lab3/meet2019y1lab3/echo.py =========
+hello world
+>>> 
+========= RESTART: /home/student/miras21_lab3/meet2019y1lab3/echo.py =========
+hello world
+>>> 
+========= RESTART: /home/student/miras21_lab3/meet2019y1lab3/echo.py =========
+hello world
+>>> 
+========= RESTART: /home/student/miras21_lab3/meet2019y1lab3/echo.py =========
+hello world
+>>> 
+========= RESTART: /home/student/miras21_lab3/meet2019y1lab3/echo.py =========
